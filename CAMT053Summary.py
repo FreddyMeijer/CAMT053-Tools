7f22be34ff3f672bcbd4c094ac31da00cfdd2cce
Freddy Meijer <freddymeijer@hotmail.com>
2023-08-03 16:27:10 +0200

    Edit logging and for future addition eddited

--- CAMT053Summary.py
+++ CAMT053Summary.py
@@ -1,8 +1,8 @@
-import csv
+import os
 import xml.etree.ElementTree as ET
 import tkinter as tk
 from tkinter import filedialog
-
+import datetime
 
 def count_xml_elements(xml_file, element_name):
     try:
@@ -29,8 +29,24 @@ def selectFile():
 if __name__ == '__main__':
     xml_file = selectFile()
     element_name = 'Stmt'
-
+    
     count = count_xml_elements(xml_file, element_name)
     output_file = xml_file.replace('.xml', '.txt')
+    vandaag=(datetime.date.today()).strftime("%d-%m-%Y")
+
+    if count == 0:
+        bericht = f"\n\n{element_name} komt helemaal niet voor. Er is waarschijnlijk iets mis gegaan met downloaden. Download het bestand opnieuw\n\n"
+    elif count == 1:
+        bericht = f"\n\n{element_name} komt 1 keer voor. Het betreft het bestand van 1 dag. Het bestand kan je inlezen in PAL21\n\n"
+    elif count > 1:
+        bericht = f"\n\n{element_name} komt meerdere keren voor ({count} keer). Donwload het bestand opnieuw vanuit de bank.\n\n"
+    else:
+        bericht = f"\n\nOnverwachte fout. Meldt de fout bij Functioneel beheer. Stuur het CAMT053 bestand mee.\n\n"
+
+    bestandsnaam = os.path.basename(xml_file)
+    headerblok=f"**************************************************************************************************************************************************************\nRAPPORT CAMT053 BESTAND\nBestand: {bestandsnaam}\nDatum: {vandaag}\n**************************************************************************************************************************************************************"
+    footerblok="**************************************************************************************************************************************************************\nEINDE RAPPORTAGE\n**************************************************************************************************************************************************************"
     with open(output_file, 'w') as f:
-        f.write(f"Het element '{element_name}' komt {count} keer voor in {xml_file}.\n")
+        f.write(headerblok)
+        f.write(bericht)
+        f.write(footerblok)
